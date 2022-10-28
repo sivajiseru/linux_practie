90961ee0ab7123eb7005bd14df581452e68b9936
sivajiseru <sivajiseru3818@gmail.com>
2022-10-28 13:21:45 +0000

    test file

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+variabletest='murali'
+echo $variabletest
+
+colgname='stmarys'
+echo $colgname
+
--- test1.sh
+++ test1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "Hello World venkat sivaji"
+abdc="venky"
+echo $a
+echo $var $var2
+
