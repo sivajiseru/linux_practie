862e74ee42a43807caebd3091303defe7db3e9a3
root <root@ip-172-31-43-38.ap-south-1.compute.internal>
2022-11-16 14:19:52 +0000

    ems pused to git

---
+++ ems.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#elementry schools in bhattiprolu mandal
+schoolname="pallikona south school"
+schoolname1="konetipura school"
+schoolname2="allamudi school"
+schoolname3="vemavaram school"
+schoolname4="bhattiprolu school"
+pallikonatotal="students total 200"
+konetipuramtotal=" students total 400"
+allamuditotal=" students total 270"
+vemavaramtotal=" students total 170"
+bhattiprolutotal=" students total 100"
+pallikonastudents="by cycle 100 numbers"
+konetipuramstudents="by cycle 250 numbers"
+allamudistudens="by cycle 170 numbers"
+vemavaramstudents="by cycle 100 numbers"
+bhattiprolustudents="by cycle 60 numbers"
+echo $schoolname 
+#$schoolname2 $schoolname3 $schoolname4
+echo $pallikonatotal
+#$allamuditotal $vemavaramtotal $bhattiprolutotal
+echo $pallikonastudents
+ #$allamudistudens $vemavaramstudents $bhattiprolustudents
+ echo "-----------------------------------------"
+echo $schoolname1
+echo $konetipuramtotal
+echo $konetipuramstudents
+echo "---------------------------------"
+echo $schoolname2
+echo $allamuditotal
+echo $allamudistudens
+echo "---------------"
+echo $schoolname3
+echo $vemavaramtotal
+echo $vemavaramstudents
+echo "-----------------"
+echo $schoolname4
+echo $bhattiprolutotal
+echo $bhattiprolustudents
