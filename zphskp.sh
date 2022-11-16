87f54a23d293b7ec331b3e4457ae4a5227e48fb3
root <root@ip-172-31-43-38.ap-south-1.compute.internal>
2022-11-16 08:03:39 +0000

    pallikona school total numbers class wise

---
+++ zphskp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#zphs pallikona class wise boys and girls total list
+tolallist6=" 40 "
+totallist7=" 35 "
+totallist8=" 60 "
+tptallist9=" 68 "
+totlalist10=" 55 "
+echo $totallist6 $totallist7 $totallist8 $totallist9 $totallist10
