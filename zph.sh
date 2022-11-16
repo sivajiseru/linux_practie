42bf4aa6e6a9fb13246daa0356843ccfed186910
root <root@ip-172-31-43-38.ap-south-1.compute.internal>
2022-11-16 07:46:51 +0000

    zphs in bhattiprolu mandal

---
+++ zph.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#zphs schoohl in bhattiprolu mandal
+schoolname=" zphs k pallikona "
+schoolname1=" zphs surepalli "
+schoolname2=" zphs bhattiprolu"
+schoolname3=" zphs velltur "
+echo $schoolname $schoolname1 $schoolname2 $schoolname3
