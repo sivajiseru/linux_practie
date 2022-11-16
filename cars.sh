f95ab92f678efd87389f9d461be369d0529dd8dc
root <root@ip-172-31-43-38.ap-south-1.compute.internal>
2022-11-16 15:29:53 +0000

    car parts

---
+++ cars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#red colour car 
+arry=("white car" "black car" "blue car" " red car")
+echo ${arry[3]}
+car="4tyres"
+car1="4doors"
+car2="ac"
+car3="2lights"
+cra4="one gaer"
+echo $car
+echo $car1
+echo $car2
+echo $car3
+echo $car4
+
