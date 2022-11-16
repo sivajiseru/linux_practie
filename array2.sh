a56e64441335d93733cf972bc055acf2d68cda2f
root <root@ip-172-31-43-38.ap-south-1.compute.internal>
2022-11-16 09:08:33 +0000

    only 4states

--- array2.sh
+++ array2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #enter the multipul state name i give only ap 
 arry=("telanga" "punjab" "goa" "andra pradesh" " kerala" "karnataka") 
-echo ${arry[3]}
+echo ${arry[@]:1:0}
+echo ${arry[@]:2:5}
+
